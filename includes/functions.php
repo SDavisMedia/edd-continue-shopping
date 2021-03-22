e548702a4ae9bd7551dedc0781d288064ccf07b7
Ashley Gibson <ashley@nosegraze.com>
2021-03-22 14:21:33 +0000

    Exit early; move default link text into edd_get_option call #10!

--- includes/functions.php
+++ includes/functions.php
@@ -16,36 +16,36 @@ if( ! defined( 'ABSPATH' ) ) exit;
  * @since       1.0.0
  */
 function eddcs_continue_shopping_link() {
-	if ( class_exists( 'Easy_Digital_Downloads' ) && ! edd_get_option( 'edd_continue_shopping' ) ) {
-		$store_page        = edd_get_option( 'edd_continue_shopping_page' );
-		$cs_text           = edd_get_option( 'edd_continue_shopping_text' );
-		$cs_link_type      = edd_get_option( 'edd_continue_shopping_link_type' );
-		$color             = edd_get_option( 'checkout_color', 'blue' );
-		$color             = ( $color == 'inherit' ) ? '' : $color;
-
-		$store_link = false;
-		if ( ! empty( $store_page ) ) {
-			$store_link = get_permalink( $store_page );
-		}
-		if ( empty( $store_link ) ) {
-			$store_link = get_post_type_archive_link( 'download' );
-		}
-
-		/*
-		 * This could still be empty if no page is selected and archives have been disabled
-		 * for the `download` post type.
-		 */
-		if ( empty( $store_link ) ) {
-			return;
-		}
-
-		$link_text = ! empty( $cs_text ) ? $cs_text : __( 'Continue Shopping', 'edd-continue-shopping' );
-		?>
-		<a href="<?php echo esc_url( $store_link ); ?>" class="edd-continue-shopping-button <?php echo 'text' == $cs_link_type ? '' : 'edd-submit button ' . $color; ?>" style="<?php echo 'text' == $cs_link_type ? 'font-size: inherit; font-weight: 400; margin-right: 4px;' : 'text-decoration: none;'; ?>">
-			<?php echo esc_html( $link_text ); ?>
-		</a>
-		<?php
+	if ( ! edd_get_option( 'edd_continue_shopping' ) ) {
+		return;
 	}
+
+	$store_page        = edd_get_option( 'edd_continue_shopping_page' );
+	$cs_text           = edd_get_option( 'edd_continue_shopping_text', __( 'Continue Shopping', 'edd-continue-shopping' ) );
+	$cs_link_type      = edd_get_option( 'edd_continue_shopping_link_type' );
+	$color             = edd_get_option( 'checkout_color', 'blue' );
+	$color             = ( $color == 'inherit' ) ? '' : $color;
+
+	$store_link = false;
+	if ( ! empty( $store_page ) ) {
+		$store_link = get_permalink( $store_page );
+	}
+	if ( empty( $store_link ) ) {
+		$store_link = get_post_type_archive_link( 'download' );
+	}
+
+	/*
+	 * This could still be empty if no page is selected and archives have been disabled
+	 * for the `download` post type.
+	 */
+	if ( empty( $store_link ) ) {
+		return;
+	}
+	?>
+	<a href="<?php echo esc_url( $store_link ); ?>" class="edd-continue-shopping-button <?php echo 'text' == $cs_link_type ? '' : 'edd-submit button ' . $color; ?>" style="<?php echo 'text' == $cs_link_type ? 'font-size: inherit; font-weight: 400; margin-right: 4px;' : 'text-decoration: none;'; ?>">
+		<?php echo esc_html( $cs_text ); ?>
+	</a>
+	<?php
 }
 add_action( 'edd_cart_footer_buttons', 'eddcs_continue_shopping_link' );
 
